6559f89024ba56e9e8bb5917b1151825e726bc56
Paulo Eduardo Dias <pauloe.jba@gmail.com>
2025-02-13 00:11:29 -0300

    Update ex6_a.js

--- chapter_6/exercicios/ex6_a.js
+++ chapter_6/exercicios/ex6_a.js
@@ -7,21 +7,34 @@ frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const clube = frm.inClube.value
-
-    clubes.push(clube)
-    console.log(clubes)
-    frm.btListarClubes.dispatchEvent(new Event("click"))
+    if (clubes.includes(clube)) {
+        alert("Esse clube já está cadastrado!")
+        return
+    } else {
+        clubes.push(clube)
+        frm.btListarClubes.dispatchEvent(new Event("click"))
+    }
 })
-
-
-resposta = ""
 
 frm.btListarClubes.addEventListener("click", () => {
     if(clubes.length == 0){
         alert("Não há clubes na lista!")
+    }                                                                 
+    resp.innerText = clubes.join("\n")
+})
+
+let resposta = ""
+
+ 
+frm.btTabelaJogos.addEventListener("click", () => {
+    if (clubes.length % 2 != 0) {
+        alert("Número de clubes não é par!")
     }
-    clubes.forEach(clube => {
-        resposta += `${clube}\n`
-    });
-    resp.innerText = resposta
+    let lista_cortada = clubes.slice(((clubes.length)/2)); // Separa o vetor clubes em 2
+    resposta = ""; // Limpa a resposta anterior
+    for (let i = 0; i < (clubes.length)/2; i++) {           
+        resposta += `${clubes[i]} x ${lista_cortada[i]}\n`;
+    }
+
+    resp.innerText = resposta; // Atualiza o texto no elemento com resp
 })
